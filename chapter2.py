d1d23eb0bbdf1d157c6badbf4a85761ff7660eb2
New <new@OrsonsdamondMBP.lan>
2019-09-24 07:35:07 +0100

    Trying out Boolean data types and if/else statements

---
+++ chapter2.py
@@ -0,0 +1,30 @@
+Chickencount = 43
+if (Chickencount == 23):
+    print("true")
+else:
+    print("false")
+
+Apples = 78
+if (Apples <= 56):
+    print ("true")
+else:
+    print ("false")
+
+    True and False #testing boolean operators 
+    not not not not not not True
+Cars = 4
+if ((Cars<8) and (8<6) ):
+    print ("true")
+else: 
+    print ("false")
+if ((3**5 == 243) and (4+4==8) and not ((5*8)==40)):
+    print ("true")
+else: 
+    print("false")
+
+print ("hello what is you name?")
+name = input()
+if (name == ("israel")):
+    print ("Hello " + str(name) + " nice to meet you!")
+else:
+    print ("Goodbye I am not permited to talk to you")
